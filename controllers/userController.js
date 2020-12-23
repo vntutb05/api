d3a2443379cf90f49aaae92e27cd3a6d8a722ea9
Tú Vũ Ngọc <vntutb@gmail.com>
2020-12-23 23:28:05 +0700

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,67 @@
 const userModel = require("../models/userModel");
 module.exports = {
-    get:(req,res,next)=>{
-        userModel.find((error,data)=>{
+    get:(req,res) => {
+        userModel.find((error,data) => {
             if(error){
                 return res.status(500).json(error);
             }
             return res.status(200).json(data);
         })
+    },
+    getAdd:(req,res) => { 
+        return res.send("Get add users");
+    },
+    postAdd:(req,res)=>{
+        let data = req.body;
+        userModel.create(data,(error,data)=>{
+            if(error){
+                return res.status(500).json(error);
+            }
+            return res.status(200).json(data);
+        })
+    },
+    getEdit:(req,res) => {
+        let id = req.params.id;
+        userModel.findById({_id:id},(error,data)=>{
+            if(error){
+                return res.status(500).json(error);
+            }
+            if(data == null){
+                return res.status(500).send("Id không chính xác");
+            }
+            return res.status(200).json(data);
+        })
+    },
+    postEdit:(req,res) => {
+        let id = req.params.id;
+        let dataUpdate = req.body;
+        userModel.find({_id:id},(error,data)=>{
+            if(data == null || error){
+                return res.send("Id không chính xác");
+            }
+            userModel.updateOne({_id:id},{$set:dataUpdate},(err,_data)=>{
+                if(err){
+                    return res.status(500).json(err);
+                }
+                return res.status(200).json(_data)
+            })
+        })
+        
+    },
+    delete:(req,res)=>{
+        let id = req.params.id;
+        userModel.find({_id:id},(error,data)=>{
+            if(data == null || error || data.length ==0){
+                return res.send("Id không chính xác");
+            }
+            userModel.deleteOne({_id:id},(err,_data)=>{
+                if(err){
+                    return res.status(500).json(err);
+                }
+                return res.status(200).json(_data);
+            })
+        });
+        
     }
+    
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 router.route("/").get(userController.get);
-
+router.route("/add").get(userController.getAdd).post(userController.postAdd);
+router.route("/edit/:id").get(userController.getEdit).post(userController.postEdit);
+router.route("/delete/:id").get(userController.delete)
 module.exports = router;
